d2cb8bf0d10a47ac4bdd2d9799070df9bc103c0a
Christopher Chen <chen1385@gmail.com>
2018-04-23 12:26:10 -0700

    added dynamic functionality to video_list, video_list_item & video_detail

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import YTSearch from 'youtube-api-search';
 // import App from './components/app';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import reducers from './reducers';
 const API_KEY = 'AIzaSyAeoVXnfl_yOrqSJpYYbyKtA_2QrP7CvAI';
 
@@ -17,20 +18,29 @@ class App extends Component {
     super(props)
 
     this.state = {
-      videos: []
+      videos: [],
+      selectedVideo: null
     }
     //run YT search when App component renders
     YTSearch({
       key: API_KEY, term: 'surfboards'
-    }, videos => this.setState({ videos }));
-  }
+    }, videos => this.setState({
+      videos,
+      selectedVideo: videos[0]
+    })
+    )
+  };
 
+
+//PRO-TIP: can define functions outside of render/sub-component (see YTSearch above) OR within a sub-component (see onVideoSelect below)
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           videos={this.state.videos}
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
         />
       </div>
     )
