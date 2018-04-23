4ff1fc337a68ad76a3afdaa4a3659c1a3f5ac6f9
Christopher Chen <chen1385@gmail.com>
2018-04-23 08:48:44 -0700

    cleaned up code for search_bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-    //generate HTML input;
-    return <input />
-};
+//functional stateless component
+
+// const SearchBar = () => {
+//     //generate HTML input;
+//     return <input />
+// };
+
+//class-based stateful component
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+        return <input onChange={(e) => console.log(e.target.value)}/>
+    }
+
+    // onInputChange(e) {
+    //     console.log(e.target.value)
+    // }
+}
 
 export default SearchBar;
