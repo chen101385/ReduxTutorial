b4732e27aa5b8942a55d7a7c830ecfbd37440424
Christopher Chen <chen1385@gmail.com>
2018-04-23 08:10:34 -0700

    added search bar component and set up webpack

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
+// import App from './components/app';
+import SearchBar from './components/search_bar';
 import reducers from './reducers';
+
+const API_KEY = 'AIzaSyAeoVXnfl_yOrqSJpYYbyKtA_2QrP7CvAI';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+//attach component to the DOM via ReactDOM
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
